29cbe528bb0f11c3ccaaa5c2ed1a057674bd4f95
eGathergood <ewan_good@hotmail.co.uk>
2020-10-21 20:21:58 +0100

    Finished tweet-feed functionality

--- src/components/TweetFeed.js
+++ src/components/TweetFeed.js
@@ -31,91 +31,118 @@ const TweetFeed = () => {
     error: {},
     isWaiting: true,
   };
-};
 
-const [state, dispatch] = useReducer(reducer, initalState);
-const { tweets, error, isWaiting } = state;
+  const [state, dispatch] = useReducer(reducer, initalState);
+  const { tweets, error, isWaiting } = state;
 
-const streamTweets = () => {
-  let socket;
+  const streamTweets = () => {
+    let socket;
 
-  if (process.env.NODE_ENV === "development") {
-    socket = socketIOClient("http://localhost:3001/");
-  } else {
-    socket = socketIOClient("/");
-  }
-
-  socket.on("connect", () => {});
-  socket.on("tweet", (json) => {
-    if (json.data) {
-      dispatch({ type: "add_tweet", payload: json });
+    if (process.env.NODE_ENV === "development") {
+      socket = socketIOClient("http://localhost:3001/");
+    } else {
+      socket = socketIOClient("/");
     }
-  });
-  socket.on("heartbeat", (data) => {
-    dispatch({ type: "update_waiting" });
-  });
-  socket.on("error", (data) => {
-    dispatch({ type: "show_error", payload: [data] });
-  });
-  socket.on("authError", (data) => {
-    console.log("data =>", data);
-    dispatch({ type: "add_errors", payload: [data] });
-  });
-};
 
-const reconnectMessage = () => {
-  const message = {
-    title: "Reconnecting",
-    detail: "Please wait while reconnecting",
-  };
-  if (error && error.detail) {
-    return (
-      <div>
-        <ErrorMessage
-          key={error.title}
-          error={error.title}
-          styleType="warning"
-        />
-        <ErrorMessage
-          key={message.title}
-          error={error.message}
-          styleType="success"
-        />
-        <Spinner />
-      </div>
-    );
-  }
-};
-
-const errorMessage = () => {
-  const { errors } = state;
-
-  if (errors && errors.length > 0) {
-    return errors.map((error) => (
-      <ErrorMessage key={error.title} error={error} styleType="negative" />
-    ));
-  }
-};
-
-const waitingMessage = () => {
-  const message = {
-    title: "Waiting",
-    detail: "Waiting for new tweets",
+    socket.on("connect", () => {});
+    socket.on("tweet", (json) => {
+      if (json.data) {
+        dispatch({ type: "add_tweet", payload: json });
+      }
+    });
+    socket.on("heartbeat", (data) => {
+      dispatch({ type: "update_waiting" });
+    });
+    socket.on("error", (data) => {
+      dispatch({ type: "show_error", payload: [data] });
+    });
+    socket.on("authError", (data) => {
+      console.log("data =>", data);
+      dispatch({ type: "add_errors", payload: [data] });
+    });
   };
 
-  if (isWaiting) {
-    return (
-      //  fragment allows to group a list of children without adding extra nodes to the DOM.
-      <React.Fragment>
+  const reconnectMessage = () => {
+    const message = {
+      title: "Reconnecting",
+      detail: "Please wait while reconnecting",
+    };
+    if (error && error.detail) {
+      return (
         <div>
           <ErrorMessage
+            key={error.title}
+            error={error.title}
+            styleType="warning"
+          />
+          <ErrorMessage
             key={message.title}
-            error={message}
+            error={error.message}
             styleType="success"
           />
+          <Spinner />
         </div>
-        <Spinner />
-      </React.Fragment>
-    );
-  }
+      );
+    }
+  };
+
+  const errorMessage = () => {
+    const { errors } = state;
+
+    if (errors && errors.length > 0) {
+      return errors.map((error) => (
+        <ErrorMessage key={error.title} error={error} styleType="negative" />
+      ));
+    }
+  };
+
+  const waitingMessage = () => {
+    const message = {
+      title: "Waiting",
+      detail: "Waiting for new tweets",
+    };
+
+    if (isWaiting) {
+      return (
+        //  fragment allows to group a list of children without adding extra nodes to the DOM.
+        <React.Fragment>
+          <div>
+            <ErrorMessage
+              key={message.title}
+              error={message}
+              styleType="success"
+            />
+          </div>
+          <Spinner />
+        </React.Fragment>
+      );
+    }
+  };
+
+  useEffect(() => {
+    streamTweets();
+  }, []);
+
+  const showTweets = () => {
+    if (tweets.length > 0) {
+      return (
+        <React.Fragment>
+          {tweets.map((tweet) => (
+            <Tweet key={tweet.data.id} json={tweet} />
+          ))}
+        </React.Fragment>
+      );
+    }
+  };
+
+  return (
+    <div>
+      {reconnectMessage()}
+      {errorMessage()}
+      {waitingMessage()}
+      {showTweets()}
+    </div>
+  );
 };
+
+export default TweetFeed;
